31e30a8d4b4560824ccdeda837939d4412eb7cdc
jarce17 <jarce@folcode.com>
2023-11-09 13:17:30 -0300

    Limpieza de archivos
    Elimino .spec

--- heroes-app/src/app/pages/home/home.component.ts
+++ heroes-app/src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { HeroesService } from 'src/app/services/heroes.service'
 
 import { Hero } from '../../models/hero'
 import { TableComponent } from '../../components/table/table.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -26,6 +27,18 @@ export class HomeComponent {
     this.heroesService.getHeroes().subscribe((response) => {
       this.heroes = response
       this.isLoading = false
+    }), () => {
+      this.isLoading = false
+      Swal.fire({
+      title: '¡Lo sentimos!',
+      text: 'Ocurrió un error al cargar los datos.',
+      icon: 'error',
+      //showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      //cancelButtonColor: '#d33',
+      confirmButtonText: 'Ok'
+      //cancelButtonText: 'Cancelar'
     })
+  }
   }
 }
